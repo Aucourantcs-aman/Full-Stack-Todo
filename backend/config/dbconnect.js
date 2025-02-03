97938f4c1512acce7c2c4737a744ed11d66f4aef
Aucourantcs-aman <aman.aucourantcs@gmail.com>
2025-02-03 14:20:10 +0530

    ..

--- backend/config/dbconnect.js
+++ backend/config/dbconnect.js
@@ -1,8 +1,15 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+
+  // aman Branch string
+
+  // const dbConnectionString =
+  //   "mongodb+srv://amaan07devloper:aman@cluster0.hav4m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+  // main branch string
   const dbConnectionString =
-    "mongodb+srv://amaan07devloper:aman@cluster0.hav4m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    "mongodb+srv://amanaucourantcs:aman@cluster0.ya1jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
   try {
     const db = await mongoose.connect(dbConnectionString);
     if (db) {
